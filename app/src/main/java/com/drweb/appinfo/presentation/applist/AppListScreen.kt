5657bd929f850ca150e1b0e7f5592b38233890a0
appz <konsultantspb@gmail.com>
2025-12-11 03:49:44 +0300

    rememberLazyListState

--- app/src/main/java/com/drweb/appinfo/presentation/applist/AppListScreen.kt
+++ app/src/main/java/com/drweb/appinfo/presentation/applist/AppListScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -30,6 +31,7 @@ fun AppListScreen(
 
     val viewModel: AppListViewModel = koinViewModel()
     val state by viewModel.uiState.collectAsStateWithLifecycle()
+    val listState = rememberLazyListState()
 
     Scaffold(
         topBar = {
@@ -53,6 +55,7 @@ fun AppListScreen(
 
                 else -> {
                     LazyColumn(
+                        state = listState,
                         modifier = Modifier.fillMaxSize()
                     ) {
                         items(state.apps, key = { it.packageName }) { app ->
